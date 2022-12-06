345baf3d9733116454a1be81da49e96774a5b1dd
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-12-06 00:11:08 +0000

    motion

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,9 @@ import Logo from '../components/Logo'
 import Button from '../components/Button'
 import TwoColLayout from '../components/TwoColLayout'
 import WorkList from '../components/WorkList'
+import SoftMotion from '../components/SoftMotion'
+import RightMotion from '../components/RightMotion'
+import LeftMotion from '../components/LeftMotion'
 
 export async function getStaticProps() {
     const data = await request({
@@ -27,72 +30,96 @@ export async function getStaticProps() {
 export default function About({data, work}) {
     return (
         <>
-        <div className='bg-pink'>
+        <div className='bg-pink overflow-x-hidden w-full'>
         <Header colour='black' bgColour='pink'/>
         <Logo url='/ROOT-logo-orange.svg'/>
         <div className='bg-grey'>
             <div className='bg-pink text-black  rounded-bl-[8rem]  md:rounded-bl-[16.5rem] mb-10'>
-                
-                
                 <TwoColLayout cols='md:grid-cols-[0.3fr_0.7fr]' border='border-t'>
+                    
                     <div className='pt-5 md:pt-10 md:border-r border-black'>
+                        <SoftMotion>
                         <div className='uppercase text-xl md:text-4xl xxl:text-6xl'>About</div>
+                        </SoftMotion>
                     </div>
-                    <div className='pt-8 md:pt-10 md:ml-8 xxl:ml-14'>
+                    
+                    <div className='pt-8 md:pt-10 md:ml-10 xxl:ml-14'>
+                        <SoftMotion>
                         <div className='mb-10 text-3xl md:text-4xl xxl:text-6xl'>{data.aboutIntro}</div>
                         <div dangerouslySetInnerHTML={{__html: data.about}} className='paragraph text-lg md:columns-2 md:text-xl xxl:text-4xl xxl:leading-tight md:w-[78%] xxl:mt-20 mb-10 md:mb-20'/>
+                        </SoftMotion>
+                        <RightMotion>
                         <div className='relative w-full h-[35vh] md:w-[90%] md:h-[70vh] xxl:h-[60vh] mb-10'>
                             <Image src={data.servicesImage.url} objectFit="cover" objectPosition="center bottom" layout='fill' className='rounded-3xl'/>
                         </div>
+                        </RightMotion>
                     </div>
                 </TwoColLayout>
                 <div id='origins' className='block relative top-[-5em] invisible'></div>
                 <TwoColLayout cols='md:grid-cols-[0.3fr_0.7fr]' border='border-t'>
                     <div className='pt-5 md:pt-10 md:border-r border-black' >
+                        <SoftMotion>
                         <div className='uppercase text-xl md:text-4xl xxl:text-6xl'>OUR<br/>ORIGINS</div>
+                        </SoftMotion>
                     </div>
-                    <div className='pt-8 md:pt-10 md:ml-8 xxl:ml-14'>
+                    <div className='pt-8 md:pt-10 md:ml-10 xxl:ml-14'>
+                        <SoftMotion>
                         <div dangerouslySetInnerHTML={{__html: data.originsText}} className='paragraph text-lg md:text-xl xxl:text-4xl xxl:leading-tight md:w-3/4 xxl:w-2/3 mb-10 md:mb-20'/>
+                        </SoftMotion>
                     </div>
                 </TwoColLayout>
                 <div className='flex justify-between items-end md:mt-[-20vh] mx-7 xxl:mx-16 pb-5'>
+                    <LeftMotion>
                     <div className='relative w-full md:w-[40vw] h-[38vh] md:h-[60vh]'>
                         <Image src={data.originsImage1.url} objectFit='contain' layout='fill'/>
                     </div>
+                    </LeftMotion>
+                    <RightMotion>
                     <div className='relative w-[25vw] h-[80vh] hidden md:block'>
                         <Image src={data.originsImage2.url} objectFit='contain' layout='fill'/>
                     </div>
+                    </RightMotion>
                 </div>
                 <div id='philosophy' className='block relative top-[-5em] invisible'></div>
                 <TwoColLayout cols='md:grid-cols-[0.3fr_0.7fr]' border='border-t'>
                     <div className='pt-5 md:pt-10 md:border-r border-black'>
+                    <SoftMotion>
                         <div className='uppercase text-xl md:text-4xl xxl:text-6xl'>OUR<br/>PHILOSOPHY</div>
+                    </SoftMotion>
                     </div>
-                    <div className='pt-8 md:pt-10 md:ml-8 xxl:ml-14'>
+                    <div className='pt-8 md:pt-10 md:ml-10 xxl:ml-14'>
+                    <SoftMotion>
                         <div dangerouslySetInnerHTML={{__html: data.philosophyText}} className='paragraph text-lg md:text-xl xxl:text-4xl xxl:leading-tight  md:w-3/4 xxl:w-2/3 mb-20'/>
+                    </SoftMotion>
                     </div>
                 </TwoColLayout>
             </div>
             <div id='services' className='block relative top-[-5em] invisible'></div>
             <TwoColLayout cols='md:grid-cols-[0.3fr_0.7fr]' border='border-t'>
                 <div className='pt-10 md:border-r border-black'>
+                    <SoftMotion>
                     <div className='uppercase text-xl md:text-4xl xxl:text-6xl'>OUR<br/>SERVICES</div>
+                    </SoftMotion>
                 </div>
-                <div className='pt-10 md:ml-8 xxl:ml-14 pb-10 xxl:pb-20'>
-                    <div dangerouslySetInnerHTML={{__html: data.servicesText}} className='paragraph text-lg md:text-xl xxl:text-4xl xxl:leading-tight md:w-3/4 xxl:w-2/3 mb-14'/>
+                <div className='pt-10 md:ml-10 xxl:ml-14 pb-10 xxl:pb-20'>
+                    <SoftMotion>
+                    <div dangerouslySetInnerHTML={{__html: data.servicesText}} className='paragraph text-lg md:text-xl xxl:text-4xl xxl:leading-tight md:w-3/4 xxl:w-2/3 mb-12'/>
                     <div className='mb-10 xxl:text-4xl xxl:leading-snug'>
                     {data.services.map((s, i) => (
                         <div key={i}>{s.title}</div>
                     ))}
                     </div>
                     <Button text='SEE OUR CLIENTS +' href='/clients' mainColour='border-black hover:bg-black hover:text-white xxl:text-4xl xxl:my-10'></Button>
+                    </SoftMotion>
                 </div>
             </TwoColLayout>
             <div className='px-7 xxl:px-16 pb-10 xxl:pb-20'>
+                <SoftMotion>
                 <div className='relative w-full h-[40vh] md:h-[95vh] xxl:h-[80vh]'>
                     <Image src={data.servicesImage.url} objectFit="cover" objectPosition="center bottom" layout='fill' className='rounded-3xl'/>
                     <div className='text-white absolute bottom-4 left-5'>{data.imageCredit}</div>
                 </div>
+                </SoftMotion>
             </div>
             
         </div>
--- pages/work/[slug].js
+++ pages/work/[slug].js
@@ -7,6 +7,9 @@ import Header from '../../components/Header'
 import Logo from '../../components/Logo'
 import TwoColLayout from '../../components/TwoColLayout'
 import WorkList from '../../components/WorkList'
+import SoftMotion from '../../components/SoftMotion'
+import RightMotion from '../../components/LeftMotion'
+import { motion } from 'framer-motion'
 
 
 
@@ -26,45 +29,61 @@ export default function CaseStudy({ data, moreProjects, work }) {
                 
                 <TwoColLayout cols='md:grid-cols-[0.25fr_0.75fr]' border='border-t'>
                     <div className='border-b md:border-b-0 pt-10 md:border-r border-black text-xl md:text-xl xxl:text-4xl'>
+                        <SoftMotion>
                         <div className='uppercase underline mr-5'>{data.client}</div>
                         <div className='uppercase mt-2 mb-10 md:mt-8'>{data.title}</div>
+                        </SoftMotion>
                     </div>
-                    <div className='pt-10 md:ml-8 text-xl xxl:ml-14'>
+                    <div className='pt-10 md:ml-10 text-xl xxl:ml-14'>
+                        <SoftMotion>
                         <div className='mb-8 md:text-lg xxl:text-4xl uppercase'>The Challenge</div>
                         <div dangerouslySetInnerHTML={{__html: data.theChallenge}} className='paragraph md:text-2xl xxl:text-6xl xxl:leading-tight md:w-3/4 mb-10 md:mb-20'/>
+                        </SoftMotion>
                     </div>
                 </TwoColLayout>
                 <div className='mx-7 md:mx-[15vw]'>
+                    <SoftMotion>
                     <div className='relative h-[40vh] md:h-[70vh]'>
                         <Image src={data.thumbnail.url} layout='fill' objectFit='cover' className='rounded-2xl'/>
                         <div className='text-white absolute bottom-4 left-6'>{data.thumbnailCredit}</div>
                     </div>
+                    </SoftMotion>
                 </div>
                 <TwoColLayout cols='md:grid-cols-[0.25fr_0.75fr]'>
                     <div className='pt-10 md:border-r border-black text-xl md:text-xl xxl:text-4xl'>
                         <div></div>
                     </div>
-                    <div className='pb-5 md:pt-10 md:ml-8 xxl:ml-14 text-xl'>
-                        <div className='mb-8 md:text-lg xxl:text-4xl uppercase'>The Ask</div>
-                        <div dangerouslySetInnerHTML={{__html: data.theAsk}} className='paragraph md:text-2xl xxl:text-6xl xxl:leading-tight md:w-3/4'/>
+                    <div className='pb-5 md:pt-10 md:ml-10 xxl:ml-14 text-xl'>
+                        <RightMotion>
+                            <div className='mb-8 md:text-lg xxl:text-4xl uppercase'>The Ask</div>
+                            <div dangerouslySetInnerHTML={{__html: data.theAsk}} className='paragraph md:text-2xl xxl:text-6xl xxl:leading-tight md:w-3/4'/>
+                        </RightMotion>
                     </div>
                 </TwoColLayout>
                 <div className='grid md:grid-cols-2 mx-7 xxl:mx-16 gap-7 mb-12'>
                     {data.images.slice(0, 2).map((img, i) => (
-                        <div key={i} className='relative h-[40vh] md:h-[65vh]'>
+                        <motion.div 
+                        initial={{ y: 100, opacity: 0 }}
+                        whileInView={{ y: 0, opacity: 1 }}
+                        transition={{ stiffness: 50, duration: 0.7, delay: `0.${i}` }}
+                        key={i} className='relative h-[40vh] md:h-[65vh]'>
                             <Image src={img.image.url} layout='fill' objectFit='cover' className='rounded-2xl'/>
                             <div className='text-white absolute bottom-4 left-6'>{img.credit}</div>
-                        </div>
+                        </motion.div>
                     ))}
                 </div>
                 <TwoColLayout cols='md:grid-cols-[0.5fr_0.5fr]' border='border-t'>
-                    <div className='pt-10 md:ml-8 xxl:ml-14 md:border-r border-black text-xl md:text-xl xxl:text-4xl'>
-                        <div className='mb-8 md:text-lg xxl:text-4xl uppercase'>What We Did</div>
-                        <div dangerouslySetInnerHTML={{__html: data.theAsk}} className='paragraph pb-4 md:text-2xl xxl:text-6xl xxl:leading-tight md:w-3/4'/>
+                    <div className='pt-10 md:ml-10 xxl:ml-14 md:border-r border-black text-xl md:text-xl xxl:text-4xl'>
+                        <SoftMotion>
+                            <div className='mb-8 md:text-lg xxl:text-4xl uppercase'>What We Did</div>
+                            <div dangerouslySetInnerHTML={{__html: data.theAsk}} className='paragraph pb-4 md:text-2xl xxl:text-6xl xxl:leading-tight md:w-3/4'/>
+                        </SoftMotion>
                     </div>
-                    <div className='border-y border-black md:border-y-0 pt-10 md:ml-8 text-xl xxl:ml-14'>
-                        <div className='mb-8 md:text-lg xxl:text-4xl uppercase'>How we did it</div>
-                        <div dangerouslySetInnerHTML={{__html: data.theAsk}} className='paragraph pb-4 md:text-2xl xxl:text-6xl xxl:leading-tight md:w-3/4'/>
+                    <div className='border-y border-black md:border-y-0 pt-10 md:ml-10 text-xl xxl:ml-14'>
+                        <SoftMotion>
+                            <div className='mb-8 md:text-lg xxl:text-4xl uppercase'>How we did it</div>
+                            <div dangerouslySetInnerHTML={{__html: data.theAsk}} className='paragraph pb-4 md:text-2xl xxl:text-6xl xxl:leading-tight md:w-3/4'/>
+                        </SoftMotion>
                     </div>
                 </TwoColLayout>
                 <div className='flex justify-between mx-7 xxl:mx-16 py-16 text-2xl xxl:text-4xl'>
